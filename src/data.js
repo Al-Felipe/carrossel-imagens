db9cafe4ef47ba262b42739b0e9ab11b55165531
Felipe Alves <felipealves3580@gmail.com>
2023-03-24 14:22:10 -0300

    Crate data

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const imageList = [
+  {
+    imageURL:
+      "https://cdn.pixabay.com/photo/2023/03/03/08/22/skulls-7827155_960_720.jpg",
+    desc: "Mãos tocando uma borboleta",
+  },
+  {
+    imageURL:
+      "https://cdn.pixabay.com/photo/2023/03/18/20/43/wolf-7861348_960_720.jpg",
+    desc: "Um lobo",
+  },
+  {
+    imageURL:
+      "https://cdn.pixabay.com/photo/2023/01/28/06/57/man-7750139_960_720.png",
+    desc: "Homem vendo a lua",
+  },
+  {
+    imageURL:
+      "https://cdn.pixabay.com/photo/2023/01/08/09/34/jellyfish-7704801_960_720.jpg",
+    desc: "Agua viva",
+  },
+  {
+    imageURL:
+      "https://cdn.pixabay.com/photo/2023/01/19/19/31/winter-7730078_960_720.jpg",
+    desc: "Uma criança na neve",
+  },
+];
